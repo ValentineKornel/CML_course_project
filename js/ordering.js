54b2581cba3178d26d02df9acd1fea62f2b90b50
Valentine Korneliuk <valentine.korneliuk17@gmail.com>
2023-05-07 21:27:11 +0300

    fixed all mistakes

--- js/ordering.js
+++ js/ordering.js
@@ -4,25 +4,19 @@ const closeButton = document.getElementById('close-button');
 
 form.addEventListener('submit', function(event) {
   event.preventDefault();
-  // Отображаем модальное окно
   modal.style.display = 'flex';
 
-  // Скрываем модальное окно через 5 секунд
   setTimeout(() => {
     modal.style.display = 'none';
     form.submit();
   }, 5000);
 });
 
-// Находим ссылку и кнопку по id
 const link = document.getElementById('submit-button');
 const button = document.getElementById('submit-form');
 
-// Назначаем обработчик событий на клик по ссылке
 link.addEventListener('click', (event) => {
-    // Отменяем стандартное поведение ссылки
     event.preventDefault();
-    // Программно вызываем событие клика на кнопке
     button.click();
 });
 
@@ -30,3 +24,4 @@ modal.addEventListener('click', function() {
     popup.style.display = 'none';
     form.submit();
 });
+
